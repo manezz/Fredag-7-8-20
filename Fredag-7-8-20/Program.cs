c83acbe95abdd33dbbe29fecec87d889d89dfc4c
Magnus <68356591+manezz@users.noreply.github.com>
2020-08-07 08:53:43 +0200

    Add project files.

---
+++ Fredag-7-8-20/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fredag_7_8_20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fornavn = "Magnus";
+
+            Console.WriteLine("Get mit navn?");
+            string svar = Console.ReadLine();
+
+            while (svar != fornavn)
+            {
+                Console.WriteLine("Forket get igen");
+                svar = Console.ReadLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
